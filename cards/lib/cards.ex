a7806fdc29dcc6211fce56748dab4abb7e9b0507
jcce <toshajo99@gmail.com>
2018-02-04 12:37:38 -0600

    writes 'deal' method that returns a Tuple

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -35,6 +35,9 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
   # def hello do
   #   "Hi there, bitches!"
   # end
